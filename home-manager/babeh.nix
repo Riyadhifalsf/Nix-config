0e16fff19e4860d153465b158a8a9e9214358947
installer <installer@gmail.com>
2026-01-22 00:30:14 +0700

    memperbaiki config home-manager

--- home-manager/babeh.nix
+++ home-manager/babeh.nix
@@ -1,14 +1,13 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-users.users.babeh = {
-  name = "babeh";
-  home = "/Users/babeh";
-};
-home-manager.users.babeh = { pkgs, ... }: {
-  home.packages = [ pkgs.atool pkgs.httpie ];
-  # programs.bash.enable = true;
+{
+  home.username = "babeh";
+  home.homeDirectory = "/home/babeh";
 
-  # The state version is required and should stay at the version you
-  # originally installed.
-  home.stateVersion = "25.11";
-};
+  # programs.zsh.enable = true;
+
+  home.packages = with pkgs; [
+    # vim
+    # git
+  ];
+}
